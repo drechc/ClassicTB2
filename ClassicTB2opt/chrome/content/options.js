f1b77653850efd4523ca19114df9d917e45d0871
Corey Drechsler <classictb2@greendeath.com>
2017-04-13 18:54:49 -0700

    Add German localization to options extension.
    Also add localized descriptions, and fix white space in script.

--- ClassicTB2opt/chrome/content/options.js
+++ ClassicTB2opt/chrome/content/options.js
@@ -34,21 +34,21 @@ var classicTB2options =
     },
 
     onLoad: function()
-	{
+    {
         window.removeEventListener("load",classicTB2options.onload,false);
         
         /* Check that the theme is being used */
         if ((classicTB2options.themeName !== "ClassicTB2") && (classicTB2options.themeName !== "ClassicTB2beta"))
             return;
 
-	    classicTB2options.prefsObserver.register();
-		
+        classicTB2options.prefsObserver.register();
+        
         /* Initialize all of the attributes */
         var obj = classicTB2options.prefs.getChildList("",{});
         for(let i = 0; i < obj.length; i += 1){
             classicTB2options.prefsObserver.observe("","",obj[i]);
         }
-	}
+    }
 };
 
 window.addEventListener("load",classicTB2options.onLoad,false);
